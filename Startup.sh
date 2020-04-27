37a53e2b5a4a4c6d78335084faef0ca938a57070
Waldenylson Silva <waldenylson.silva@yahoo.com.br>
2020-04-26 21:21:11 -0300

    testar

--- Startup.sh
+++ Startup.sh
@@ -5,6 +5,8 @@
 # Autor			: Waldenylson Silva <waldenylsonwpss@fab.mil.br>
 # Manutenção	: Waldenylson Silva <waldenylsonwpss@fab.mil.br>
 # Agradecimento	: Diego Varejão	<varejaodfav@fab.mil.br>
+#				  Versão Anterior modificada para adequação ao DTCEA-AR
+#
 # Atualização	: 23/04/2020
 #
 # CINDACTA III / Divisão Técnica
@@ -35,7 +37,7 @@
 # v032020-0.1 11-03-2020, Diego Varejão
 #	- Versão inicial
 # v042020-0.2 23-04-2020, Waldenylson Silva
-#   - Segunda Versão
+#   - Segunda Versão <DTCEA-AR>
 
 #AMHSVM='AMHS2020'
 #TARISVM='TARIS2020'
@@ -52,7 +54,8 @@ DATA=`date "+%m-%d-%Y %T"`
 #ESTADOAMHS=`vboxmanage showvminfo "$AMHSVM" | grep -c "running (since"`
 #ESTADOTARIS=`vboxmanage showvminfo "$TARISVM" | grep -c "running (since"`
 
-ESTADOTESTE=`vboxmanage showvminfo "$TESTEVM" | grep -c "running (since"`
+#ESTADOTESTE=`vboxmanage showvminfo "$TESTEVM" | grep -c "running (since"`
+ESTADOTESTE=1
 
 # Manipula as VMs =============================================================#
 # $1 - Operação a ser executada na VM                                          #
@@ -62,6 +65,8 @@ function ManipularVM()
 {
 	case "$1" in
 		start)
+			#sleep 15
+
 			notify-send -i /home/operador/.config/tiop/icones/exit.png -t \
 			30000 \
 			"Iniciando $2" "O sistema virtual do $2 está sendo iniciado, \
@@ -71,6 +76,24 @@ function ManipularVM()
 			manutenção\n" >> /tmp/testeLogScript.log #/tiop/Logs/vmlog.txt
 
 			VBoxManage startvm $2  >> /tmp/testeLogScript.log 2>&1 #/tiop/Logs/vmlog.txt 2>&1
+
+			if [ "$?" -ne 0 ]; then 
+
+				notify-send -i /home/operador/.config/tiop/icones/cancel.svg -t \
+				100000 \
+				"Falha ao inicializar $2" "Ocorreu uma falha ao inicializar o \
+				sistema virtual. Favor entrar em contato com a TIOp pelo ramal \
+				8293 e reportar o ocorrido...!"
+
+				exit 1
+			fi
+
+			sleep 30
+
+			notify-send -i /home/operador/.config/tiop/icones/checked.svg \
+			-t 100000 "Inicialização concluída!" \
+			"Acesse o $2 pressionando Ctrl + Alt + $3 e retorne ao linux \
+			pressionando Ctrl + Alt + 1"
 		;;
 		stop)
 			notify-send -i /home/operador/.config/tiop/icones/exit.png -t \
@@ -93,121 +116,12 @@ function ManipularVM()
 			manutenção\n" >> /tmp/testeLogScript.log #/tiop/Logs/vmlog.tx
 
 			ManipularVM stop $2
-			sleep 10
+			sleep 15
 			ManipularVM start $2
 		;;
 	esac	
 }
 
-echo $ESTADOTESTE
-
-
-
-if [ $ESTADOMV -eq 1 ]; then # VM RUNNING
-
-	
-	
-	printf "\n$DATA Desligamento da máquina virtual pelo script de \
-	manutenção\n" >> /tiop/Logs/vmlog.txt
-	
-	# Desliga a máquina virtual do AMHS antes de executar qualquer ação
-	ManipularVM stop $AMHSVM 
-	
-	
-	sleep 30
-
-	if [ $GATILHO -eq 0 ]; then
-	
-		notify-send -i /home/operador/.config/tiop/icones/exit.png -t \
-		30000 \
-		"Desligando TARIS" "O sistema virtual do TARIS está sendo desligado, \
-		por favor aguarde até todo o sistema ser desativado."
-		
-		printf "\n$DATA Desligamento da máquina virtual pelo script de \
-		manutenção\n" >> /tiop/Logs/vmlog.txt
-	
-		# Desliga a máquina virtual do TARIS antes de Desligar o Computador
-		VBoxManage controlvm $NOMEVM2 acpipowerbutton >> \
-		/tiop/Logs/vmlog.txt 2>&1
-
-		notify-send -i /home/operador/.config/tiop/icones/exit.png -t \
-		10000 \
-		"Desligando computador" "Seu computador será desligado agora"
-		
-		printf "$DATA Desligamento do computador pelo script de \
-		manutenção\n" >> /tiop/Logs/hostlog.txt
-		
-		sleep 3
-		
-		systemctl poweroff
-		
-	elif [ $GATILHO -eq 2 ]; then
-	
-		printf "$DATA Reinicialização da máquina virtual pelo script de \
-		manutenção\n" >> /tiop/Logs/vmlog.txt
-	
-		#openbox --exit
-		VBoxManage startvm $NOMEVM
-		
-	else
-
-		notify-send -i /home/operador/.config/tiop/icones/exit.png -t \
-		30000 \
-		"Desligando TARIS" "O sistema virtual do TARIS está sendo desligado, \
-		por favor aguarde até todo o sistema ser desativado."
-		
-			printf "\n$DATA Desligamento da máquina virtual pelo script de \
-		manutenção\n" >> /tiop/Logs/vmlog.txt
-	
-		# Desliga a máquina virtual do TARIS antes de Reiniciar o Computador
-		VBoxManage controlvm $NOMEVM2 acpipowerbutton >> \
-		/tiop/Logs/vmlog.txt 2>&1
-	
-		notify-send -i /home/operador/.config/tiop/icones/exit.png -t \
-		10000 \
-		"Reiniciando computador" "Seu computador será reiniciado agora"
-		
-		printf "$DATA Reinicialização do computador pelo script de \
-		manutenção\n" >> /tiop/Logs/hostlog.txt
-		
-		sleep 3
-		
-		systemctl reboot
-	fi
-
-else
-	if [ $GATILHO -eq 0 ]; then
-	
-		notify-send -i /home/operador/.config/tiop/icones/exit.png -t \
-		10000 \
-		"Desligando computador" "Seu computador será desligado agora"
-		
-		sleep 3
-		
-		printf "$DATA Desligamento do computador pelo script de \
-		manutenção\n" >> /tiop/Logs/hostlog.txt
-		
-		systemctl poweroff
-		
-	elif [ $GATILHO -eq 2 ]; then
-	
-		printf "$DATA Inicialização da máquina virtual pelo script de \
-	manutenção\n" >> /tiop/Logs/vmlog.txt
-	
-		#openbox --exit
-		vboxmanage startvm $NOMEVM
-		
-	else
-	
-		notify-send -i /home/operador/.config/tiop/icones/exit.png -t \
-		10000 \
-		"Reiniciando computador" "Seu computador será reiniciado agora"
-		
-		printf "$DATA Reinicialização do computador pelo script de \
-	manutenção\n" >> /tiop/Logs/hostlog.txt
-		
-		sleep 3
-		
-		systemctl reboot
-	fi
+if [ $ESTADOTESTE -eq 1 ]; then # VM RUNNING
+	ManipularVM $1 $2 $3
 fi
